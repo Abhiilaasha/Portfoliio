0c07580b6b856701c5dfaa88e6efe3ac3ddb2ced
Abhilaasha <129691817+Abhiilaasha@users.noreply.github.com>
2024-01-24 16:08:25 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Abhiilaasha",
   resume:
-    "https://drive.google.com/file/d/1Y5TOmr7sVhOFyUTtjoXDWoaNPb706wvd/view?usp=sharing",
+    "https://drive.google.com/file/d/1qEeRbqOgUoixBkDgzNqhwuZmlr-r7oNT/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/abhilaasha19/",
   insta: "https://instagram.com/_abhi._19",
 };
